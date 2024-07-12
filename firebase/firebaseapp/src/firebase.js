dfb26eab018dcae5f0f3e69a4423ebda2d781fdf
suhail khan <khansohail015@gmail.com>
2024-07-12 09:00:17 +0530

    chages

--- firebase/firebaseapp/src/firebase.js
+++ firebase/firebaseapp/src/firebase.js
@@ -12,6 +12,7 @@ const firebaseConfig = {
   messagingSenderId: "781260307446",
   appId: "1:781260307446:web:b86cb32bd59d4cfeb925d7",
   databaseURL:"https://blogs-app-c9fa1-default-rtdb.firebaseio.com/"
+  
 };
 
 // Initialize Firebase
