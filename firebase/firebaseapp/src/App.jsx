a1e0fbba1915aad66bba18c64fdd19ee49743114
suhail khan <khansohail015@gmail.com>
2024-07-17 20:32:29 +0530

    signup with google

--- firebase/firebaseapp/src/App.jsx
+++ firebase/firebaseapp/src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { signOut,getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "./firebase";
 
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
+const auth = getAuth(app);
+
 function App() {
+  const [user, setUser] = useState(null);
+  const userSignout = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        console.log("user logout");
+        setUser(null);
+      }
+    });
+  };
+  useEffect(() => {
+    userSignout();
+  }, []);
+
+
   return (
     <>
-    <h1>Signup Page</h1>
-      <Signup />
-    <h1>Login Page</h1>
+    
+    {user?<div><h1>Hello {user.email}</h1> <button onClick={()=>signOut(auth)}>LogOut</button></div>:<div>
+        <h1>Signup Page</h1>
+        <Signup />
+        <h1>Login Page</h1>
 
-      <Login />
+        <Login />
+      </div>}
+      
     </>
   );
 }
